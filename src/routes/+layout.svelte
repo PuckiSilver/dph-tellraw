a8e9d5e43ed415c39bd62e0615c2a528c2195b0f
Silabear <56885288+Silabear@users.noreply.github.com>
2023-12-02 20:19:14 +0000

    let it be light(er)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,8 @@
 	import tippy from "tippy.js";
 </script>
 
-<div class="font-lexend bg-zinc-900 text-white min-h-screen h-full">
-    <div class="flex items-center space-x-2 p-4 bg-zinc-900">
+<div class="font-lexend bg-zinc-800 text-white min-h-screen h-full">
+    <div class="flex items-center space-x-2 p-4 bg-zinc-800">
         <div class="flex items-center space-x-2 flex-grow">
             <img src="/dph.png" class="h-8"/>
             <h1 class="text-2xl"><b>Datapack Hub</b> Tools</h1>
@@ -15,18 +15,18 @@
         <a href="https://datapackhub.net" class="text-blue-300">To datapackhub.net -> </a>
     </div>
     <div class="flex flex-grow h-[92vh]">
-        <div class="flex flex-col space-y-2 bg-zinc-900 p-2">
-            <a href="/" class="p-2 bg-zinc-800 rounded-lg group" use:tippy={{content:"Home", placement:"right",}}>
+        <div class="flex flex-col space-y-2 bg-zinc-800 p-2">
+            <a href="/" class="p-2 bg-zinc-700 rounded-lg group" use:tippy={{content:"Home", placement:"right",}}>
                 <IconHome size={45} class="text-orange-500 group-hover:text-orange-400 transition-all"/>
             </a>
-            <a href="tellraw" class="p-2 bg-zinc-800 rounded-lg group" use:tippy={{content:"/tellraw generator", placement:"right",}}>
+            <a href="tellraw" class="p-2 bg-zinc-700 rounded-lg group" use:tippy={{content:"/tellraw generator", placement:"right",}}>
                 <IconAlphabetLatin size={45} class="text-blue-500 group-hover:text-blue-400 transition-all"/>
             </a>
-            <a class="p-2 bg-zinc-800 rounded-lg group" use:tippy={{content:"Book generator", placement:"right",}}>
+            <a class="p-2 bg-zinc-700 rounded-lg group" use:tippy={{content:"Book generator", placement:"right",}}>
                 <IconBook2 size={45} class="text-blue-500 group-hover:text-blue-400 transition-all"/>
             </a>
         </div>
-        <div class="bg-zinc-950 p-3 mr-2 rounded-lg flex-grow overflow-y-auto" use:autoAnimate>
+        <div class="bg-zinc-900 p-3 mr-2 rounded-lg flex-grow overflow-y-auto" use:autoAnimate>
             <slot />
         </div>
     </div>
